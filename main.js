8dc19ed124ede7218620e8bf6a8b83196d53d072
Thorsten Pohl <thorsten.pohl@lht.dlh.de>
2023-07-17 12:04:03 +0200

    feat - register every 5 minutes chore - silence of the logs

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const env = {
-
+  AREA: process.env.AREA || 'house',
   MQTT_HOST: process.env.MQTT_HOST || '192.168.178.100',
   MQTT_TOPIC: `house/power/${process.env.POWERTYPE || 'main'}`,
   POWER_TYPE: process.env.POWERTYPE || 'main',
   MODE: process.env.MODE || 'serial',
   SOCKET_HOST: process.env.SOCKET_HOST || '192.168.178.100',
   SOCKET_PORT: parseInt(process.env.SOCKET_PORT || '2002'),
-  SERIAL_PATH: process.env.SERIAL_PATH || '/dev/ttyUSB0'
+  SERIAL_PATH: process.env.SERIAL_PATH || '/dev/ttyUSB0',
+  DEBUG: process.env.DEBUG === 'true',
+  DATA_INTERVAL: parseInt(process.env.DATA_INTERVAL || '30000'), // Default 30 Sec
+  REGISTER_INTERVAL: parseInt(process.env.DATA_INTERVAL || '300000'), // Default 5 mins (300 Sec)
 };
 
 let client;
+
 let clientConnect = function () {
   console.warn('Client Connect not configured');
 };
@@ -40,6 +44,10 @@ const mqttclient = mqtt.connect(`mqtt://${env.MQTT_HOST}`);
 
 let received = '';
 let lastUpdate = Date.parse('01 Jan 1970 00:00:00 GMT');
+let lastRegister = Date.parse('01 Jan 1970 00:00:00 GMT');
+let updateCounter = 0;
+let registerCounter = 0;
+
 client.on('data', data => {
   received += data;
   const messages = received.split('!');
@@ -69,15 +77,25 @@ client.on('data', data => {
           }
         }
         if (Object.keys(dataPoint).length >= 8) {
-          if (Date.now() > (lastUpdate + 30000)) {
-            console.log('Sending Data Point');
-            //console.log('Sending Data Point', dataPoint);
+          if (Date.now() > (lastUpdate + env.DATA_INTERVAL)) {
+            if (env.DEBUG) {
+              console.log('Sending Data Point', dataPoint);
+            }
+            //Reregister
+            registerConfig();
+            
+            // Send
             lastUpdate = Date.now();
             mqttclient.publish(env.MQTT_TOPIC, JSON.stringify(dataPoint));
+
+            // Update Counter
+            updateCounter ++;
+            if (updateCounter % 100 === 0){
+              console.log(`Sent ${updateCounter} messages`);
+            }
           }
         }
-
-        //  console.log('Message ', message);
+        
         received = '';
       }
     }
@@ -90,35 +108,54 @@ client.on('close', () => {
 
 
 function connect() {
-
   // Send Autoconfig to Home Assistant
-
-
-  registerConfig('total');
-  registerConfig('total_day');
-  registerConfig('total_night');
-  registerConfig('total_1d', 'total');
-  registerConfig('total_7d', 'total');
-  registerConfig('total_30d', 'total');
-  registerConfig('total_365d', 'total');
-  registerConfig('current_power', 'measurement', 'W', 'power');
-
+  registerConfig();
   clientConnect();
 }
 
-function registerConfig(measure, state_class = 'total_increasing', unit = 'kWh', device_class = 'energy') {
+function registerConfig() {
+  if (Date.now() > (lastRegister + env.DATA_INTERVAL)) {
+    lastRegister = Date.now();
+    registerSubConfig('total');
+    registerSubConfig('total_day');
+    registerSubConfig('total_night');
+    registerSubConfig('total_1d', 'total');
+    registerSubConfig('total_7d', 'total');
+    registerSubConfig('total_30d', 'total');
+    registerSubConfig('total_365d', 'total');
+    registerSubConfig('current_power', 'measurement', 'W', 'power');
+
+    // Update Register Counter
+    registerCounter ++;
+    if (registerCounter % 100 === 0){
+      console.log(`Registered ${registerCounter} times`);
+    }
+  }
+}
+
+function registerSubConfig(measure, state_class = 'total_increasing', unit = 'kWh', device_class = 'energy') {
   const config = {
-    'object_id': `energy_${env.POWER_TYPE}_${measure}`,
-    'entity_id': `energy_${env.POWER_TYPE}_${measure}`,
+    object_id: `energy_${env.POWER_TYPE}_${measure}`,
+    entity_id: `energy_${env.POWER_TYPE}_${measure}`,
+    unique_id: `energy_${env.POWER_TYPE}_${measure}`,
     name: `Energy ${env.POWER_TYPE} ${measure}`,
     state_topic: env.MQTT_TOPIC,
     state_class: state_class,
     device_class: device_class,
+    device: {
+      identifiers: `logarex-meter-${env.POWER_TYPE}`,
+      name: `Electricity Meter ${env.POWER_TYPE}`,
+      model: `Logarex`,
+      suggested_area: `${env.AREA}`,
+      via_device: `logarex-mqtt-bridge-${env.POWER_TYPE}`
+    },
     unit_of_measurement: unit,
     value_template: `{{ value_json.${measure} }}`
   };
   mqttclient.publish(`homeassistant/sensor/${config.object_id}/config`, JSON.stringify(config));
-  console.log('Registering Config', config);
+  if (env.DEBUG) {
+    console.log('Registering Config', config);
+  }
   return config;
 }
 
